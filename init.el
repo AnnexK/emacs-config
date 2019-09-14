bb667894cdac4aef437e0e02ff8fe9fea3b9ef96
frolikk <roadto3kmmr@gmail.com>
2019-09-14 16:36:30 +0700

    Changed theme to dracula

--- init.el
+++ init.el
@@ -7,6 +7,21 @@
 
 (load "~/.emacs.d/misc/load-directory.el")
 
-(defvar *theme* 'cyberpunk)
+(defvar *theme* 'dracula)
 
 (load "~/.emacs.d/elpa.el")
+(load-directory "~/.emacs.d/languages/")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (dracula-theme smartparens slime markdown-mode elpy cyberpunk-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
