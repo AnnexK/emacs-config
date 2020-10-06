c95051fe6220d271689851dab03769fa5dda591b
TrotzigerEmil <roadto3kmmr@gmail.com>
2020-10-07 03:21:51 +0700

    Disabled package-enable-at-startup for slightly faster startup

--- elpa.el
+++ elpa.el
@@ -1,6 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/") t)
+(setq package-enable-at-startup nil)
 (package-initialize)
 
 (defvar required-packages (list 'slime
