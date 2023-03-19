ea0f4539fff1e9e4f592f11361e83102d4a3ed96
frolikk <roadto3kmmr@gmail.com>
2023-03-19 20:17:50 +0700

    refactor: remove elpa packages configs

--- elpa-packages-config/company.el
+++
@@ -1,8 +0,0 @@
-(add-hook 'after-init-hook 'global-company-mode)
-
-(setq company-idle-delay 0)
-(setq company-minimum-prefix-length 1)
-
-(setq company-selection-wrap-around t)
-
-(company-tng-configure-default)
