125c20a2a5be9ae62f1ef3c839eecfd7b56303d5
AnnexK <roadto3kmmr@gmail.com>
2023-05-13 20:36:37 +0700

    feat: add projectile

---
+++ packages-config/projectile.el
@@ -0,0 +1,8 @@
+(req-package projectile
+  :init
+  (projectile-mode +1)
+  :config
+  (setq projectile-project-search-path '(("~/" . 1)))
+  :bind
+  (:map projectile-mode-map
+	("C-c p" . projectile-command-map)))
