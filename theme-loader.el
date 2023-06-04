274fc6c671f9a7c06f0896cc1f0978f704e5de00
frolikk <roadto3kmmr@gmail.com>
2023-06-04 16:12:14 +0700

    feat: add theme loader

---
+++ theme-loader.el
@@ -0,0 +1,26 @@
+;;; theme-loader -- Theme loader utility -*- lexical-binding: t -*-
+;;; Commentary:
+;;; This package provides apply-theme function
+;;; that either loads theme or appends a hook that applies theme to frame,
+;;; depending on whether Emacs is run in daemon mode or not.
+
+;;; Code:
+
+(defun apply-theme (theme)
+  "Apply theme.
+If Emacs is run in daemon mode, add a hook that applies THEME to client frame.
+If Emacs is run in server mode, just apply THEME."
+  (if (member theme custom-known-themes)
+      (if (daemonp)
+	  (add-hook 'after-make-frame-functions (make-theme-loader theme))
+	(enable-theme theme))
+    (message "Theme %s is not available; skipping apply-theme" theme)))
+
+(defun make-theme-loader (theme)
+  (let ((my-theme theme))
+    (lambda (frame) (select-frame frame) (enable-theme my-theme))))
+
+
+(provide 'theme-loader)
+;;; theme-loader.el ends here
+
