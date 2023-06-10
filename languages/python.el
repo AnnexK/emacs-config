dbc1dd566ad7fc208c24b75a11c1d2bd3de190a0
frolikk <roadto3kmmr@gmail.com>
2023-06-10 10:38:24 +0700

    feat: add flycheck python-ruff checker

--- languages/python.el
+++ languages/python.el
@@ -8,6 +8,32 @@
 (require 'pyvenv)
 (require 'flycheck)
 
+(flycheck-define-checker python-ruff
+  "A Python syntax and style checker using the ruff utility.
+To override the path to the ruff executable, set
+`flycheck-python-ruff-executable'.
+See URL `http://pypi.python.org/pypi/ruff'."
+  
+  :command ("ruff"
+            "--format=text"
+            (eval (when buffer-file-name
+                    (concat "--stdin-filename=" buffer-file-name)))
+            "-")
+  :standard-input t
+  :error-filter (lambda (errors)
+                  (let ((errors (flycheck-sanitize-errors errors)))
+                    (seq-map #'flycheck-flake8-fix-error-level errors)))
+  :next-checkers ((warning . python-mypy))
+  :error-patterns
+  ((warning line-start
+            (file-name) ":" line ":" (optional column ":") " "
+            (id (one-or-more (any alpha)) (one-or-more digit)) " "
+            (message (one-or-more not-newline))
+            line-end))
+  :modes python-mode)
+
+(add-to-list 'flycheck-checkers 'python-ruff)
+
 (setq-default flycheck-disabled-checkers
 	      (cons 'python-pycompile (default-value flycheck-disabled-checkers)))
 
@@ -23,5 +49,6 @@
 
 (add-hook 'projectile-after-switch-project-hook #'projectile-activate-venv-on-project-switch)
 
+(provide 'flycheck-ruff)
 (provide 'python)
 ;;; python.el ends here
