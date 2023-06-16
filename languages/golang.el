041d94f635c6d56c50fca082d043d031a79bb0f0
frolikk <roadto3kmmr@gmail.com>
2023-06-17 01:09:51 +0700

    feat: add projectile go.mod project type

---
+++ languages/golang.el
@@ -0,0 +1,8 @@
+(require 'projectile)
+
+(projectile-register-project-type 'golang-module '("go.mod" "go.sum")
+				  :project-file "go.mod"
+				  :compile "go build"
+				  :test "go test"
+				  :test-prefix "test_"
+				  :run "go run")
