217356e1516b38c13e1a5661d55e4ab470996100
frolikk <roadto3kmmr@gmail.com>
2023-06-17 01:08:08 +0700

    refactor: open project root in dired on project switch

--- packages-config/projectile.el
+++ packages-config/projectile.el
@@ -4,6 +4,7 @@
   (projectile-mode +1)
   :config
   (setq projectile-project-search-path '(("~/" . 1)))
+  (setq projectile-switch-project-action #'projectile-dired)
   :bind
   (:map projectile-mode-map
 	("C-c p" . projectile-command-map)))
