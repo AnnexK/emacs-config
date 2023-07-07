5834fc6d73a62a01515da0c8328b09b9accfa460
frolikk <roadto3kmmr@gmail.com>
2023-07-08 02:22:51 +0700

    refactor: actually load themes when applying themes

--- theme-loader.el
+++ theme-loader.el
@@ -10,15 +10,15 @@
   "Apply theme.
 If Emacs is run in daemon mode, add a hook that applies THEME to client frame.
 If Emacs is run in server mode, just apply THEME."
-  (if (member theme custom-known-themes)
+  (if (member theme (custom-available-themes))
       (if (daemonp)
 	  (add-hook 'after-make-frame-functions (make-theme-loader theme))
-	(enable-theme theme))
+	(load-theme theme t))
     (message "Theme %s is not available; skipping apply-theme" theme)))
 
 (defun make-theme-loader (theme)
   (let ((my-theme theme))
-    (lambda (frame) (select-frame frame) (enable-theme my-theme))))
+    (lambda (frame) (select-frame frame) (load-theme my-theme t))))
 
 
 (provide 'theme-loader)
