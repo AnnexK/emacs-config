f0240d250667c10eb69a46ae7359d4f671cf3a16
AnnexK <roadto3kmmr@gmail.com>
2024-03-10 23:51:39 +0700

    feat: reenable lsp for python

--- languages/python.el
+++ languages/python.el
@@ -8,6 +8,9 @@
 (require 'pyvenv)
 (require 'flycheck)
 
+(setq lsp-pylsp-configuration-sources ["pylsp-mypy"])
+(setq lsp-pylsp-plugins-black-enabled t)
+
 (flycheck-define-checker python-ruff
   "A Python syntax and style checker using the ruff utility.
 To override the path to the ruff executable, set
@@ -25,7 +28,7 @@ See URL `http://pypi.python.org/pypi/ruff'."
   :error-filter (lambda (errors)
                   (let ((errors (flycheck-sanitize-errors errors)))
                     (seq-map #'flycheck-flake8-fix-error-level errors)))
-  :next-checkers ((warning . python-mypy))
+  :next-checkers ((t . python-mypy))
   :error-patterns
   ((warning line-start
             (file-name) ":" line ":" (optional column ":") " "
@@ -34,17 +37,16 @@ See URL `http://pypi.python.org/pypi/ruff'."
             line-end))
   :modes python-mode)
 
-(add-to-list 'flycheck-checkers 'python-ruff)
 
 (setq-default flycheck-disabled-checkers
-	      (cons 'python-pycompile (default-value flycheck-disabled-checkers)))
+			  (append flycheck-disabled-checkers '(python-pylint python-mypy python-pyright python-pycompile)))
 
 (defun projectile-activate-venv-on-project-switch ()
   (let* ((project-root (projectile-project-root))
-	 (venv-dir (concat project-root ".venv"))
-	 (is-pyproject (eql (projectile-project-type) 'python-toml)))
+		 (venv-dir (concat project-root ".venv"))
+		 (is-pyproject (eql (projectile-project-type) 'python-toml)))
     (if (and (file-exists-p venv-dir) is-pyproject)
-	(pyvenv-activate venv-dir)
+		(pyvenv-activate venv-dir)
       (pyvenv-deactivate))))
 
 (add-hook 'projectile-after-switch-project-hook #'projectile-activate-venv-on-project-switch)
--- packages-config/lsp-mode.el
+++ packages-config/lsp-mode.el
@@ -3,5 +3,6 @@
 (req-package lsp-mode
   :init (setq lsp-keymap-prefix "C-c l")
   :commands lsp
-  :hook ((go-mode . lsp)))
+  :config (lsp-configure-buffer)
+  :hook ((go-mode . lsp) (python-mode . lsp)))
 
