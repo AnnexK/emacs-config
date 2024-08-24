ecc19fdc733e7012a0934cb9c0fad5c15331efea
AnnexK <roadto3kmmr@gmail.com>
2024-08-24 16:21:42 +0700

    feat: enable projectile-run to get user input

--- packages-config/projectile.el
+++ packages-config/projectile.el
@@ -5,6 +5,7 @@
   :config
   (setq projectile-project-search-path '(("~/" . 1)))
   (setq projectile-switch-project-action #'projectile-dired)
+  (setq projectile-run-use-comint-mode t)
   :bind
   (:map projectile-mode-map
 	("C-c p" . projectile-command-map)))
